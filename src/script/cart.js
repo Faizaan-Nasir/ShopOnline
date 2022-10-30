a00f0b3d4386dfbf8861d245db945d0771c1f568
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-10-30 19:47:54 +0400

    added shopping cart functionality (wishlist)

---
+++ src/script/cart.js
@@ -0,0 +1,13 @@
+let fav = JSON.parse(`[${localStorage.favourite}]`)
+let data = JSON.parse(localStorage.getItem("data") || "[]");
+for (j = 1; j < fav.length; j++) {
+    for (i = 0; i <= data.length; i++) {
+        if (fav[j]['p_id'] == String(i)) {
+            let div = document.createElement("div");
+            div.id = `display${i}`;
+            div.classList.add("card");
+            document.getElementById("display-cart").appendChild(div)
+            div.innerHTML = `<div class="store-container"><div class="store"><img src="${data[i]["p_img"]}" ><h3>${data[i]["p_name"]}</h3> </div><div class="description box"><div class="des">${data[i]["p_description"]}</div></div>`
+        }
+    }
+}
