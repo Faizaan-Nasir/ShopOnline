73123721f360317138ba65a8ad788df94e208f8e
amazinglySK <shashwat07ktm@gmail.com>
2022-10-30 13:11:15 +0400

    Cleaning up some js code

--- src/script/load.js
+++ src/script/load.js
@@ -1,86 +1,94 @@
-const api_url = "https://sheetdb.io/api/v1/6lmkthf76fddi"
-let data
-let temp
+const api_url = "https://sheetdb.io/api/v1/6lmkthf76fddi";
+
+// <please fill in what this block does>
 var t_date = new Date().toLocaleDateString("de-DE");
-var l_date = localStorage.date
-const check = String(localStorage.data)
-if (check == 'undefined' || l_date != t_date) {
-    getData()
-}
-else {
+var l_date = localStorage.date;
+const check = String(localStorage.data);
+
+// Checking for data in cache (LS)
+if (check == "undefined" || l_date != t_date) {
+    getData();
+} else {
     data = JSON.parse(localStorage.getItem("data") || "[]");
-    load()
-}
-async function getData() {
-    const response = await fetch(api_url)
-    temp = await response.json()
-    localStorage.setItem("data", JSON.stringify(temp));
-    data = JSON.parse(localStorage.getItem("data") || "[]");
-    load()
-}
-function load() {
-    if (data.length >= 0) {
-        const main = document.getElementById("main")
-        const loader = document.getElementById("loader")
-        loader.style.display = "none"
-        main.style.display = "block"
-    }
-    else {
-        alert("The server threw an error. Contact the store owner immediately.")
-    }
-    var a = 0
-    var b = 0
-    var c = 0
-    for (i = 0; i < data.length; i++) {
-        div = document.createElement("div");
-        div.id = `display${i}`
-        div.style.marginLeft = "2%"
-        div.style.marginRight = "2%"
-        div.style.marginBottom = "2%"
-        div.style.marginTop = "1%"
-        if (`${data[i]['p_cat']}` == "Fastners" && a < 5) {
-            document.getElementById("Fastners").appendChild(div);
-            document.getElementById(`display${i}`).innerHTML = `<div class="store"><img src="${data[i]['p_img']}"><h3>${data[i]['p_name']}</h3> </div>`
-            a = a + 1
-        }
-        else if (`${data[i]['p_cat']}` == "Paints" && b < 5) {
-            document.getElementById("Paints").appendChild(div);
-            document.getElementById(`display${i}`).innerHTML = `<div class="store"><img src="${data[i]['p_img']}"><h3>${data[i]['p_name']}</h3> </div>`
-            b = b + 1
-        }
-        else if (`${data[i]['p_cat']}` == "Electrical" && c < 5) {
-            document.getElementById("Electrical").appendChild(div);
-            document.getElementById(`display${i}`).innerHTML = `<div class="store"><img src="${data[i]['p_img']}"><h3>${data[i]['p_name']}</h3> </div>`
-            c = c + 1
-        }
-    }
-    var d = 0
-    for (j = data.length - 1; j >= 0; j--) {
-        div = document.createElement("div");
-        div.id = `display${j}`
-        div.style.marginLeft = "2%"
-        div.style.marginRight = "2%"
-        div.style.marginBottom = "4%"
-        if (d < 5) {
-            document.getElementById("Latest").appendChild(div);
-            document.getElementById(`display${j}`).innerHTML = `<div class="store"><img src="${data[j]['p_img']}" class="latest-img"><h3>${data[j]['p_name']}</h3> </div>`
-            d = d + 1
-        }
-    }
-    e = 0
-    for (k = data.length - 1; k >= 0; k--) {
-        div = document.createElement("div");
-        div.id = `display${k}`
-        div.style.marginLeft = "2%"
-        div.style.marginRight = "2%"
-        div.style.marginBottom = "4%"
-        if (data[k]['p_spc'] == "yes" && e < 5) {
-            document.getElementById("Top-Picks").appendChild(div);
-            document.getElementById(`display${k}`).innerHTML = `<div class="store-container"><div class="store"><img src="${data[k]['p_img']}" class="spc-img"><h3>${data[k]['p_name']}</h3> </div><div class="description"></div>`
-            e = e + 1
-        }
-    }
-    localStorage.date = t_date
+    load();
 }
 
-// <div class="des">${data[k]['p_description']}</div>
+// gets the data from the API
+async function getData() {
+    const response = await fetch(api_url);
+    let temp = await response.json();
+    localStorage.setItem("data", JSON.stringify(temp));
+    load();
+}
+
+// Uses the data and does hydrates the page
+function load() {
+    let data = JSON.parse(localStorage.getItem("data") || "[]");
+
+    if (data.length >= 0) {
+        const main = document.getElementById("main");
+        const loader = document.getElementById("loader");
+        loader.style.display = "none";
+        main.style.display = "block";
+    } else {
+        alert(
+            "The server threw an error. Contact the store owner immediately."
+        );
+    }
+
+    let a = 0;
+    let b = 0;
+    let c = 0;
+
+    // Main categories
+    for (i = 0; i < data.length; i++) {
+        // Creating a new div
+        div = document.createElement("div");
+        div.id = `display${i}`;
+        div.classList.add("card");
+
+        // Adding them according to their categories
+        if (`${data[i]["p_cat"]}` == "Fastners" && a < 5) {
+            document.getElementById("Fastners").appendChild(div);
+            a = a + 1;
+        } else if (`${data[i]["p_cat"]}` == "Paints" && b < 5) {
+            document.getElementById("Paints").appendChild(div);
+            b = b + 1;
+        } else if (`${data[i]["p_cat"]}` == "Electrical" && c < 5) {
+            document.getElementById("Electrical").appendChild(div);
+            c = c + 1;
+        }
+
+        div.innerHTML = `<div class="store"><img src="${data[i]["p_img"]}"><h3>${data[i]["p_name"]}</h3> </div>`;
+    }
+
+    let d = 0;
+
+    // Latest
+    for (j = data.length - 1; j >= 0; j--) {
+        div = document.createElement("div");
+        div.id = `display${j}`;
+        div.classList.add("card");
+        if (d < 5) {
+            document.getElementById("Latest").appendChild(div);
+            div.innerHTML = `<div class="store"><img src="${data[j]["p_img"]}" class="latest-img"><h3>${data[j]["p_name"]}</h3> </div>`;
+            d = d + 1;
+        }
+    }
+
+    let e = 0;
+
+    // Top picks
+    for (k = data.length - 1; k >= 0; k--) {
+        div = document.createElement("div");
+        div.id = `display${k}`;
+        div.classList.add("card");
+        if (data[k]["p_spc"] == "yes" && e < 5) {
+            document.getElementById("Top-Picks").appendChild(div);
+            div.innerHTML = `<div class="store-container"><div class="store"><img src="${data[k]["p_img"]}" class="spc-img"><h3>${data[k]["p_name"]}</h3> </div><div class="description"></div>`;
+            e = e + 1;
+        }
+    }
+
+    localStorage.date = t_date;
+}
--- src/script/submit.js
+++ src/script/submit.js
@@ -1,18 +1,21 @@
 var form = document.getElementById("sheetdb-form");
+
+async function submitData(url) {
+    fetch(url, {
+        method: "POST",
+        body: new FormData(form),
+    })
+        .then((response) => response.json())
+        .then((jsonRes) => {
+            // Checking if the request was successful
+            if (jsonRes.status == "201") {
+                document.getElementById("verified").innerHTML =
+                    "Data Submitted Successfully";
+            }
+        });
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    async function getData() {
-        const api_url = "https://sheetdb.io/api/v1/rqdrbx6lcpktk"
-        const response = await fetch(api_url)
-        const data = await response.json()
-        fetch(form.action, {
-            method: "POST",
-            body: new FormData(document.getElementById("sheetdb-form")),
-        })
-            .then((response) => response.json())
-            .then((html) => {
-                document.getElementById("verified").innerHTML = "Data Submitted Successfully";
-            });
-    }
-    getData()
-})
+    submitData();
+});
